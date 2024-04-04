aeab5ba543e4b3ace9d1f6fd23b3eb98f692fc91
Patriziano <onahpatrick75@gmail.com>
2024-04-04 22:41:43 +0000

    Basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A module containing solution to annotated float add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float and returns their float value
+    """
+    return a + b
