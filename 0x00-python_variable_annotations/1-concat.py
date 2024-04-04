482bb8ee85da4db7416f697953f1df0409e98f1a
Patriziano <onahpatrick75@gmail.com>
2024-04-04 22:49:12 +0000

    concat annotation

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A module containing a type annotated concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Adds two str together"""
+    return str1 + str2
