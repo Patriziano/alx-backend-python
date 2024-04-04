4726bbbf4a3548fcdb9f99a36991294f96b9880f
Patriziano <onahpatrick75@gmail.com>
2024-04-04 22:55:46 +0000

    floor annotation of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+A module containing a floor method that returns the floor of
+a float passed to it
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of n"""
+    return math.floor(n)
